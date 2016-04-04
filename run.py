82535e6740554898b03da541c45507a6205aa9d5
Piotr Wójcik <piotr_to@op.pl>
2016-04-05 00:27:10 +0200

    main window

--- run.py
+++ run.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python
 # coding: utf-8
+from PyQt4 import QtGui
 
-from modules.helpers import FlowLayout
 from ui.Window import Window
 
 
 
-
-
-
-
-
 if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    window = Window()
+    sys.exit(app.exec_())
--- ui/Window.py
+++ ui/Window.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python
 # coding: utf-8
+from PyQt4.QtGui import QMainWindow
 
 from MainWindow import Ui_MainWindow
 
 
-class Window(Ui_MainWindow):
-    pass
+class WindowUi(Ui_MainWindow):
+    def setupUi(self, MainWindow):
+        super(WindowUi, self).setupUi(MainWindow)
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.ui = WindowUi()
+        self.ui.setupUi(self)
