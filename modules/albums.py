bca6ebd4fa5223cb2dcf47b8884854fe367ef799
Piotr Wójcik <piotr_to@op.pl>
2016-04-05 12:11:16 +0200

    albumy

--- modules/albums.py
+++ modules/albums.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python
 # coding: utf-8
+
+
+class AbstractAlbum(object):
+    def __init__(self):
+        self.pictures = []
+
+    def get_pictures(self):
+        return self.pictures
+
+    def remove_picture(self, pic):
+        self.pictures.remove(pic)
+
+    def add_picture(self, pic):
+        self.pictures.append(pic)
+
+
+class Album(AbstractAlbum):
+    pass
+
+
+class View(AbstractAlbum):
+    pass
