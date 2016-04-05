1dbd0be9719c1d098d8866a66b911ee033e4267a
Piotr Wójcik <piotr_to@op.pl>
2016-04-05 12:15:11 +0200

    moduł ładujący dane od użytkownika

--- modules/io.py
+++ modules/io.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python
 # coding: utf-8
+
+
+def load_picture(path):
+    raise NotImplemented
+
+
+def save_picture(path):
+    raise NotImplemented
+
+
+def copy_picture(path):
+    raise NotImplemented
